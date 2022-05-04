dabb1a3d17af7fa0accae624896f2b405606f3b7
Gionelle Tribiana <gionellef@gmail.com>
2022-05-04 20:27:19 +0800

    Working poc

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# streamlit-poc
+
+This is a POC for the VP of Student Affairs that aims to show what Executive Dashboards are for and the value it can bring to the University.
--- app.py
+++ app.py
@@ -10,12 +10,13 @@ import plotly.express as px
 
 chart = functools.partial(st.plotly_chart, use_container_width=True)
 COMMON_ARGS = {
-    "color": "symbol",
+    "color": "course",
     "color_discrete_sequence": px.colors.sequential.Greens,
     "hover_data": [
-        "account_name",
+        "college",
         "percent_of_account",
         "quantity",
+        "current_population",
         "total_gain_loss_dollar",
         "total_gain_loss_percent",
     ],
@@ -81,140 +82,134 @@ def filter_data(
 
 
 def main() -> None:
-    st.header("Student Affairs Dashboard :bar_chart:")
+    st.header("Academic Affairs Dashboard :bar_chart:")
 
-    with st.expander("How to Use This"):
+    with st.expander("Purpose"):
         st.write(Path("README.md").read_text())
 
-    st.subheader("Upload your CSV from Fidelity")
-    uploaded_data = st.file_uploader(
-        "Drag and Drop or Click to Upload", type=".csv", accept_multiple_files=False
-    )
+#    st.subheader("Upload your CSV from Fidelity")
+#    uploaded_data = st.file_uploader(
+#        "Drag and Drop or Click to Upload", type=".csv", accept_multiple_files=False
+#    )
 
-    if uploaded_data is None:
-        st.info("Using example data. Upload a file above to use your own data!")
+#    if uploaded_data is None:
+#        st.info("Using example data. Upload a file above to use your own data!")
         uploaded_data = open("example.csv", "r")
-    else:
-        st.success("Uploaded your file!")
+#    else:
+#        st.success("Uploaded your file!")
 
     df = pd.read_csv(uploaded_data)
-    with st.expander("Raw Dataframe"):
-        st.write(df)
+#    with st.expander("Raw Dataframe"):
+#        st.write(df)
 
     df = clean_data(df)
-    with st.expander("Cleaned Data"):
-        st.write(df)
+#    with st.expander("Cleaned Data"):
+#        st.write(df)
+    
+    st.sidebar.subheader("Select Dashboards:")
 
-    st.sidebar.subheader("Filter Displayed Accounts")
+    # accounts = list(df.account_name.unique())
+    # account_selections = st.sidebar.multiselect(
+    #     "Select Accounts to View", options=accounts, default=accounts
+    # )
+    # st.sidebar.subheader("Filter Displayed Tickers")
 
-    accounts = list(df.account_name.unique())
-    account_selections = st.sidebar.multiselect(
-        "Select Accounts to View", options=accounts, default=accounts
+    # symbols = list(df.loc[df.account_name.isin(account_selections), "symbol"].unique())
+    # symbol_selections = st.sidebar.multiselect(
+    #     "Select Ticker Symbols to View", options=symbols, default=symbols
+    # )
+
+    dashboard = list(df.upm_affairs.unique())
+    account_selections = st.sidebar.selectbox(
+        "Select Dashboards to View", options=dashboard
     )
-    st.sidebar.subheader("Filter Displayed Tickers")
+    # st.sidebar.subheader("Filter Displayed Tickers")
 
-    symbols = list(df.loc[df.account_name.isin(account_selections), "symbol"].unique())
-    symbol_selections = st.sidebar.multiselect(
-        "Select Ticker Symbols to View", options=symbols, default=symbols
-    )
+    # symbols = list(df.loc[df.upm_affairs.isin(account_selections), "symbol"].unique())
+    # symbol_selections = st.sidebar.multiselect(
+    #     "Select Ticker Symbols to View", options=symbols, default=symbols
+    # )
 
-    df = filter_data(df, account_selections, symbol_selections)
-    st.subheader("Selected Account and Ticker Data")
-    cellsytle_jscode = JsCode(
-        """
-    function(params) {
-        if (params.value > 0) {
-            return {
-                'color': 'white',
-                'backgroundColor': 'forestgreen'
-            }
-        } else if (params.value < 0) {
-            return {
-                'color': 'white',
-                'backgroundColor': 'crimson'
-            }
-        } else {
-            return {
-                'color': 'white',
-                'backgroundColor': 'slategray'
-            }
-        }
-    };
-    """
-    )
+    # df = filter_data(df, account_selections, symbol_selections)
+    # st.subheader("Enrollment Data")
+    # cellsytle_jscode = JsCode(
+    #     """
+    # function(params) {
+    #     if (params.value > 0) {
+    #         return {
+    #             'color': 'white',
+    #             'backgroundColor': 'forestgreen'
+    #         }
+    #     } else if (params.value < 0) {
+    #         return {
+    #             'color': 'white',
+    #             'backgroundColor': 'crimson'
+    #         }
+    #     } else {
+    #         return {
+    #             'color': 'white',
+    #             'backgroundColor': 'slategray'
+    #         }
+    #     }
+    # };
+    # """
+    # )
 
-    gb = GridOptionsBuilder.from_dataframe(df)
-    gb.configure_columns(
-        (
-            "last_price_change",
-            "total_gain_loss_dollar",
-            "total_gain_loss_percent",
-            "today's_gain_loss_dollar",
-            "today's_gain_loss_percent",
-        ),
-        cellStyle=cellsytle_jscode,
-    )
-    gb.configure_pagination()
-    gb.configure_columns(("account_name", "symbol"), pinned=True)
-    gridOptions = gb.build()
+    # gb = GridOptionsBuilder.from_dataframe(df)
+    # gb.configure_columns(
+    #     (
+    #         "last_price_change",
+    #         "total_gain_loss_dollar",
+    #         "total_gain_loss_percent",
+    #         "today's_gain_loss_dollar",
+    #         "today's_gain_loss_percent",
+    #     ),
+    #     cellStyle=cellsytle_jscode,
+    # )
+    # gb.configure_pagination()
+    # gb.configure_columns(("account_name", "symbol"), pinned=True)
+    # gridOptions = gb.build()
 
-    AgGrid(df, gridOptions=gridOptions, allow_unsafe_jscode=True)
-
+    # AgGrid(df, gridOptions=gridOptions, allow_unsafe_jscode=True)
+    
+    
+    
     def draw_bar(y_val: str) -> None:
-        fig = px.bar(df, y=y_val, x="symbol", **COMMON_ARGS)
+        fig = px.bar(df, y=y_val, x="course", **COMMON_ARGS)
         fig.update_layout(barmode="stack", xaxis={"categoryorder": "total descending"})
         chart(fig)
 
-    account_plural = "s" if len(account_selections) > 1 else ""
-    st.subheader(f"Value of Account{account_plural}")
-    totals = df.groupby("account_name", as_index=False).sum()
-    if len(account_selections) > 1:
-        st.metric(
-            "Total of All Accounts",
-            f"${totals.current_value.sum():.2f}",
-            f"${totals.total_gain_loss_dollar.sum():.2f}",
-        )
+    account_plural = "s" # if len(account_selections) > 1 else ""
+    st.subheader(f"Statistics of Student{account_plural}")
+    totals = df.groupby("college", as_index=False).sum()
+    #if len(account_selections) > 1:
+    st.metric(
+        "Total Enrolled Students",
+        f"{totals.current_population.sum():.0f}",
+        f"{totals.total_gain_loss_dollar.sum():.0f}",
+    )
     for column, row in zip(st.columns(len(totals)), totals.itertuples()):
         column.metric(
-            row.account_name,
-            f"${row.current_value:.2f}",
-            f"${row.total_gain_loss_dollar:.2f}",
+            row.college,
+            f"{row.current_population:.0f}",
+            f"{row.total_gain_loss_dollar:.0f}",
         )
 
     fig = px.bar(
         totals,
-        y="account_name",
-        x="current_value",
-        color="account_name",
+        y="college",
+        x="current_population",
+        color="college",
         color_discrete_sequence=px.colors.sequential.Greens,
     )
     fig.update_layout(barmode="stack", xaxis={"categoryorder": "total descending"})
     chart(fig)
 
-    st.subheader("Value of each Symbol")
-    draw_bar("current_value")
-
-    st.subheader("Value of each Symbol per Account")
-    fig = px.sunburst(
-        df, path=["account_name", "symbol"], values="current_value", **COMMON_ARGS
-    )
-    fig.update_layout(margin=dict(t=0, b=0, l=0, r=0))
-    chart(fig)
-
-    st.subheader("Value of each Symbol")
-    fig = px.pie(df, values="current_value", names="symbol", **COMMON_ARGS)
-    fig.update_layout(margin=dict(t=0, b=0, l=0, r=0))
-    chart(fig)
-
-    st.subheader("Total Value gained each Symbol")
-    draw_bar("total_gain_loss_dollar")
-    st.subheader("Total Percent Value gained each Symbol")
-    draw_bar("total_gain_loss_percent")
 
 
 if __name__ == "__main__":
     st.set_page_config(
-        "Fidelity Account View by Gerard Bentley",
+        "UPM Executive Dashboard",
         "📊",
         initial_sidebar_state="expanded",
         layout="wide",
